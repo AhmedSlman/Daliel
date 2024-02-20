f954176046c056db1baf694b6099ab1f6a69faff
AhmedSlman <ahmedslman151@gmail.com>
2024-02-20 14:28:05 +0200

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Daliel());
+}
+
+class Daliel extends StatelessWidget {
+  const Daliel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
