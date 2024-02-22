c5b3f9d587c3162f9fc4fcc85ae41c8566d42693
AhmedSlman <ahmedslman151@gmail.com>
2024-02-22 19:55:16 +0200

    welcom

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,3 +1,6 @@
+import 'package:dalel/core/services/service_locator.dart';
+import 'package:dalel/features/auth/presentation/auth_cubit/auth_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/auth/presentation/views/sign_in_view.dart';
@@ -17,11 +20,17 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: "/signUp",
-      builder: (context, state) => const SignUpView(),
+      builder: (context, state) => BlocProvider(
+        create: (context) => getIt<AuthCubit>(),
+        child: const SignUpView(),
+      ),
     ),
     GoRoute(
       path: "/signIn",
-      builder: (context, state) => const SignInView(),
+      builder: (context, state) => BlocProvider(
+        create: (context) => getIt<AuthCubit>(),
+        child: const SignInView(),
+      ),
     ),
   ],
 );
